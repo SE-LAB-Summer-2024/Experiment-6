8e7152da4398dd4c534ffcffdb6252cbba42fb4c
mohamadreza <mohamadrza.pr@gmail.com>
2024-08-23 17:20:06 +0330

    FEAT: add the bus strategy func

--- src/main/java/graph/strategy/BusStrategy.java
+++ src/main/java/graph/strategy/BusStrategy.java
@@ -1,2 +1,14 @@
-package graph.strategy;public class BusStrategy {
+package graph.strategy;
+
+import graph.Graph;
+import graph.Node;
+
+public class BusStrategy implements TransportationStrategy {
+
+    @Override
+    public int calculateTime(Graph graph, Node start, Node end, int trainTime) {
+        graph.resetVisits();
+        graph.dijkstra(start);
+        return end.getDistance();
+    }
 }
